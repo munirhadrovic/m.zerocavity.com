b51e68fa89d6a514e090b7f263d0df71cdf09490
neswill1 <nenadkonatar@live.ac.me>
2015-04-17 10:16:39 +0200

    futer

--- first/index.php
+++ first/index.php
@@ -127,7 +127,7 @@
 		</div>
 		<div data-role="footer" data-theme="<?php echo $theme ?>" data-position="fixed">
 
-            <h1 style="color: black;">Powered by <a href="http://mobilegrows.com/">MobileGrows </a>|<a href="http://www.zerocavity.com/" rel="external"> Desktop Site</a></h1>
+            <h1 style="color: black;">Powered by <a href="http://mobilegrows.com/" rel="external" >MobileGrows </a>|<a href="http://www.zerocavity.com/" rel="external"> Desktop Site</a></h1>
 		</div>
 		<!-- page 1 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////-->
 		<div data-role="page" id="page1" data-theme="<?php echo $theme ?>">
